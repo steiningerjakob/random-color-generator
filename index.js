decf9c23da6550ccc2dda05109f778b09d03e6e8
“Jakob <steiningerjakob@gmail.com>
2021-05-04 17:09:04 +0200

    Add 80% version

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const randomColor = require('randomcolor'); // import the script
-const color = randomColor(); // a hex code for an attractive color
-const chalk = require('chalk');
+const chalk = require('chalk'); // imports chalk script
 
-console.log(chalk.hex(color)`
+const color = randomColor(); // a hex code for an attractive color
+
+const Output = `
 ###############################
 ###############################
 ###############################
@@ -11,5 +12,11 @@ console.log(chalk.hex(color)`
 #####                     #####
 ###############################
 ###############################
-###############################
-`);
+###############################`; // this is the output
+
+if (!process.argv[2]) {
+  console.log(chalk.hex(color).bold(Output));
+} else {
+  console.log(chalk.keyword(process.argv[2])(Output));
+}
+// this calls the output in the correct color
