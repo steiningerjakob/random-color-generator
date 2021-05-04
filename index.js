9925f074c1bfbdbc4d15f811c890d64532e34911
“Jakob <steiningerjakob@gmail.com>
2021-05-04 16:16:59 +0200

    Add first version with functionality

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+const randomColor = require('randomcolor'); // import the script
+const color = randomColor(); // a hex code for an attractive color
+const chalk = require('chalk');
+
+console.log(chalk.hex(color)`
+###############################
+###############################
+###############################
+#####                     #####
+#####        ${color}      #####
+#####                     #####
+###############################
+###############################
+###############################
+`);
